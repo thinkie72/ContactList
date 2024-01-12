d057217e82228159a7d9eb0da144ca83d9359290
thinkie72 <tyler.hinkie@menloschool.org>
2024-01-12 13:45:21 -0800

    Person class finished

--- src/Person.java
+++ src/Person.java
@@ -1,3 +1,29 @@
 public class Person {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
 
+    public Person(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + " " + lastName + " - #" + phoneNumber;
+    }
 }
+
